e80ada7b774f296d3e93453aeeca5fb1f6089f81
amichaizelig <amichai@zelig.co.il>
2021-12-26 18:33:44 +0200

    add 1.py

---
+++ 1.py
@@ -0,0 +1,30 @@
+print("hello")
+my_var = 10
+print(my_var)
+my_name = "Amichai"
+print(my_name)
+is_true = False
+# my comment about this variable
+my_list = ["Amichai","Zelig", 45, True, my_name]
+print(my_list[2])
+my_dict = {"name" : "Amichai", "lname": "Zelig", "is_married" : True}
+print(my_dict["is_married"])
+print(my_dict.keys())
+print(list(my_dict.keys()))
+
+my_number = 5 * 2
+my_other = 5 * "Amichai"
+print(my_other)
+
+if my_number == 10:
+    print(my_number)
+
+fname = "Amichai"
+lname = "Zelig"
+print("hello " + fname + " " + lname)
+print(f"hello {fname} {lname}")
+print("hello %s %s" % (lname, fname))
+
+
+
+
